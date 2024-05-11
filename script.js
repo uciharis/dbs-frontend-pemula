93610b4f035a5c9ab3bd93ffcd5777a2e9d4f8af
uciharis <harisnop@gmail.com>
2024-05-11 22:38:37 +0700

    ralat bukan metod tapi properti dom

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let gambar = document.getElementById('gambar');
 gambar.setAttribute('width','300px'); // hanya atribut dan strktur
-gambar.style.width='300px'; //ubah riil
+gambar.style.width='300px'; //ubah riil :p
 gambar.setAttribute('height','215px'); // hanya atribut dan struktur html
-gambar.style.height='215px' //ubah rill
+gambar.style.height='215px' //ubah rill :p
