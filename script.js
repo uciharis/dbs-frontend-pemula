ad6bf8899ce4e4d65e2a71d831c8e1ecd6dba5ac
uciharis <harisnop@gmail.com>
2024-05-11 22:37:16 +0700

    ubah rill dengan metod style

---
+++ script.js
@@ -0,0 +1,5 @@
+let gambar = document.getElementById('gambar');
+gambar.setAttribute('width','300px'); // hanya atribut dan strktur
+gambar.style.width='300px'; //ubah riil
+gambar.setAttribute('height','215px'); // hanya atribut dan struktur html
+gambar.style.height='215px' //ubah rill
