5a20cdf9419663ba49f3ea837cbdf6692379cbe0
harisnop.koding <harisnop.koding@gmail.com>
2024-06-02 18:00:12 +0700

    deteksi jumlah klik =7, keluar pesan tersembunyi

--- EVENT/script.js
+++ EVENT/script.js
@@ -4,3 +4,18 @@ function welcome(){
     kontent.removeAttribute('hidden');
 }
 document.body.onload = welcome;
+
+function adding(){
+    let konter = document.getElementById('counter');
+    konter.innerText++;
+
+    if(konter.innerText == 7){
+        let hiddenMessage = document.createElement('h3');
+        hiddenMessage.innerText = 'selamat anda menemukan hadiah! ';
+        let gambarHidden = document.createElement('img');
+        gambarHidden.setAttribute('src',https://i.ibb.co/0V49VRZ/catto.jpg');
+        let kontent = document.querySelector('.content');
+        kontent.appendChild(hiddenMessage).appendChild(gambarHidden);
+    }
+}
+document.getElementById('tambahButton').onclick = adding;
