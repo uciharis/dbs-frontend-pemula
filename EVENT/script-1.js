c9ecd702d2bbcfb30972a04d34bfe43ddf36cfb1
uciharis <harisnop@gmail.com>
2024-06-04 23:33:02 +0700

    custom event

--- EVENT/script-1.js
+++ EVENT/script-1.js
@@ -1,7 +1,15 @@
 let gantiCaption = new Event('gantiCaption'); // inisiasi sebuah event
 
 window.addEventListener('load',function(){
-    let tombol = document.getElementById('tombo')
+    let tombol = document.getElementById('tombol')
     tombol.addEventListener('gantiCaption',customEventHandler);
-    tombol.addEventListener('click', function(){})
-})
+    tombol.addEventListener('click', function(){
+        tombol.dispatchEvent(gantiCaption)
+    })
+});
+
+function customEventHandler(event){
+    console.log('event ' + event.type + ' telah dijalankan');
+    let caption = document.getElementById('caption');
+    caption.innerText = 'anda membangkitkan custom event'
+}
