228678c0e55a4c06e6825c3cf7776621004ae206
uciharis <harisnop@gmail.com>
2024-06-06 21:52:12 +0700

    commented console.log

--- EVENT/script-2.js
+++ EVENT/script-2.js
@@ -1,9 +1,9 @@
 let submitAction = document.getElementById('formDataDiri');
 submitAction.addEventListener('submit', function(event){
     let inputNama = document.getElementById('inputNama').value;
-    console.log(inputNama);
+    //console.log(inputNama);
     let inputDomisili = document.getElementById('inputDomisili').value;
-    console.log(inputDomisili);
+    //console.log(inputDomisili);
     let hiddenMessage = `halo, ${inputNama}. bagaimana kondisi di ${inputDomisili}?`
 
     document.getElementById('messageAfterSubmit').innerText = hiddenMessage;
