0b25c400c63f4c8c809b88e9088571bc5f030943
uciharis <harisnop@gmail.com>
2024-06-06 21:30:12 +0700

    event submit form

---
+++ EVENT/script-2.js
@@ -0,0 +1,12 @@
+let submitAction = document.getElementById('formDataDiri');
+submitAction.addEventListener('submit', function(event){
+    let inputNama = document.getElementById('inputNama').value;
+    console.log(inputNama);
+    let inputDomisili = document.getElementById('inputDomisili').value;
+    console.log(inputDomisili);
+    let hiddenMessage = `halo, ${inputNama}. bagaimana kondisi di ${inputDomisili}?`
+
+    document.getElementById('messageAfterSubmit').innerText = hiddenMessage;
+    event.preventDefault;
+
+});
