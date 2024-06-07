a7089efb06c4c40db5a3fa1a3af47c2c3ca940ba
uciharis <harisnop@gmail.com>
2024-06-07 22:27:29 +0700

    perbaikan div

--- EVENT/script-3.js
+++ EVENT/script-3.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     let inputMaxLengthOnLoad = document.getElementById('inputNama').maxLength;
-})
+    document.getElementById('sisaKarakter').innerText=inputMaxLengthOnLoad;
+
+    document.getElementById('inputNama').addEventListener('input',function(){
+        let jumlahKarakterDiketik = document.getElementById('inputNama').value.length;
+        let jumlahKarakterMaksimal = document.getElementById('inputNama').maxLength;
+
+        console.log('jumlah char diketik: ', jumlahKarakterDiketik);
+        console.log('jumlah char Maks: ', jumlahKarakterDiketik);
+        let sisaKarakterUpdate = jumlahKarakterMaksimal-jumlahKarakterDiketik;
+        document.getElementById('sisaKarakter').innerText = sisaKarakterUpdate.toString();
+
+        if (sisaKarakterUpdate === 0) {
+            document.getElementById('sisaKarakter').innerText = 'batas char habis!';
+        } else if (sisaKarakterUpdate <= 5){
+            document.getElementById('notifikasiSisaKarakter').style.color = 'red';
+        } else {
+            document.getElementById('notifikasiSisaKarakter').style.color = 'black';
+        }
+    });
+});
